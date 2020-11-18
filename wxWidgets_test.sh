5a8cf2673254e84ce64d3851e77674cc6aa59f40
Talipov S.N <proffix4@gmail.com>
2020-11-18 10:39:16 +0600

    wxWidgets

---
+++ wxWidgets_test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+echo [*] Test information:
+wx-config --cxxflags
+wx-config --libs
+
+echo [*] Create a test program...
+`wx-config --cxx --cxxflags` -o wxWidgets_test *.cpp `wx-config --libs`
+
+echo [*] Run the assembled program...
+./wxWidgets_test
